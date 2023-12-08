fb7cbe54cce0f3a284d62207d9effb1049ab2cdf
Mritunjay <mupadhyay00@gmail.com>
2023-12-09 01:00:00 +0700

    🔧 fix(app.js): add support for user routes to enable user-related functionality 🚀 feat(users.route.js): create user router to handle user-related requests 🎉 feat(users.controller.js): add getAllUsers controller function to handle getting all users request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import chatRouter from "./routes/chat.routes.js";
+import userRouter from "./routes/users.route.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 import { createServer } from "http";
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // configure static file to save images locally
 
 app.use("/chats", chatRouter);
+app.use("/users", userRouter);
 
 // common error handling middleware
 app.use(errorHandler);
