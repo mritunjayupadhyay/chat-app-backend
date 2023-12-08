f05ad1ed456507a82e6bd4ea6ac9d03ad09af997
Mritunjay <mupadhyay00@gmail.com>
2023-12-09 00:30:08 +0700

    📝 chore(.env): add .env file to store CORS_ORIGIN environment variable for better configuration management
    🔧 fix(app.js): add cors middleware to enable Cross-Origin Resource Sharing (CORS) with the specified origin
    
    📦 feat(package.json): add dotenv package as a dependency to enable reading environment variables from .env file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import chatRouter from "./routes/chat.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
@@ -6,11 +8,19 @@ import { createServer } from "http";
 const app = express();
 const httpServer = createServer(app);
 
+// global middlewares
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // configure static file to save images locally
 
-app.use("/api/chats", chatRouter);
+app.use("/chats", chatRouter);
 
 // common error handling middleware
 app.use(errorHandler);
